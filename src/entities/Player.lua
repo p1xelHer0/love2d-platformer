dbc10d67ee245c464f5e50483e7acfad1ab75628
Pontus Nagy <pontusnagy@gmail.com>
2018-01-26 19:18:23 +0100

    Removed extra frame from legdude.

--- src/entities/Player.lua
+++ src/entities/Player.lua
@@ -49,11 +49,11 @@ local function Player()
 	local g = anim8.newGrid(16, 16, image:getWidth(), image:getHeight(), 0)
 
 	local animations = {
-		stand = anim8.newAnimation(g('1-3', 1), 0.5),
-		slide = anim8.newAnimation(g('8-1', 1), 0.5),
-		jump = anim8.newAnimation(g('5-1', 1), 0.5),
-		crouch = anim8.newAnimation(g('6-1', 1), 0.5),
-		fall = anim8.newAnimation(g('7-1', 1), 0.5),
+		stand = anim8.newAnimation(g('1-2', 1), 0.5),
+		slide = anim8.newAnimation(g('7-1', 1), 0.5),
+		jump = anim8.newAnimation(g('4-1', 1), 0.5),
+		crouch = anim8.newAnimation(g('5-1', 1), 0.5),
+		fall = anim8.newAnimation(g('6-1', 1), 0.5),
 	}
 
 	entity:add(Animation(animations))
