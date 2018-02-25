16c0919ae54ea43aa74d0bd849ed3a9d052f0bb0
Pontus Nagy <pontusnagy@gmail.com>
2018-02-25 10:48:14 +0100

    tilemap wip

--- assets/tiles/tilesets/8px/ruins.tsx
+++ assets/tiles/tilesets/8px/ruins.tsx
@@ -3,17 +3,30 @@
  <image source="ruins_tilemap.png" width="64" height="64"/>
  <terraintypes>
   <terrain name="Base" tile="0"/>
+  <terrain name="Base_Small" tile="0"/>
   <terrain name="Grass_1" tile="0"/>
-  <terrain name="Mark_1" tile="0"/>
-  <terrain name="Mark_2" tile="0"/>
   <terrain name="Grass_2" tile="0"/>
+  <terrain name="Grass_3" tile="0"/>
+  <terrain name="Grass_4" tile="0"/>
+  <terrain name="Grass_5" tile="0"/>
+  <terrain name="Mark_2" tile="0"/>
+  <terrain name="Mark_1" tile="0"/>
  </terraintypes>
  <tile id="0" terrain=",,,0"/>
- <tile id="1" terrain=",,0,"/>
- <tile id="4" terrain="1,1,1,1"/>
- <tile id="5" terrain="4,4,4,4"/>
- <tile id="8" terrain=",0,,"/>
- <tile id="9" terrain="0,,,"/>
- <tile id="20" terrain="3,3,3,3"/>
- <tile id="29" terrain="2,2,2,2"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="4,4,4,4"/>
+ <tile id="4" terrain="2,2,2,2"/>
+ <tile id="5" terrain="3,3,3,3"/>
+ <tile id="6" terrain="5,5,5,5"/>
+ <tile id="7" terrain="6,6,6,6"/>
+ <tile id="8" terrain=",0,,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,,0,"/>
+ <tile id="16" terrain=",0,,"/>
+ <tile id="17" terrain="0,0,,"/>
+ <tile id="18" terrain="0,,,"/>
+ <tile id="20" terrain="7,7,7,7"/>
+ <tile id="24" terrain="1,1,1,1"/>
+ <tile id="29" terrain="8,8,8,8"/>
 </tileset>
